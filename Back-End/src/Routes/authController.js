691877e031dbac827b2472e8b18a7cb725cfc95b
ilyabukhantsov <ilyabukhantsov5@gmail.com>
2025-05-09 00:25:30 +0300

    h

--- Back-End/src/Routes/authController.js
+++ Back-End/src/Routes/authController.js
@@ -1,7 +1,26 @@
-//authController
+const User = require("../models/User");
+const Role = require("../models/Role");
+const bcrypt = require("bcryptjs");
+
 class authController {
   async registration(req, res) {
     try {
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "Пользователь с таким именем уже существует" });
+      }
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: "USER" });
+      const user = new User({
+        username,
+        password: hashPassword,
+        roles: [userRole.value],
+      });
+      await user.save();
+      return res.json({ message: "Пользователь успешно зарегистрирован" });
     } catch (e) {}
   }
   async login(req, res) {
