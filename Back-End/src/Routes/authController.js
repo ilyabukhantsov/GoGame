e489bcec7bab737d619b010297baa10b7670d33f
ilyabukhantsov <ilyabukhantsov5@gmail.com>
2025-05-10 13:49:25 +0300

    Like i dont like that everything works good rn, i hope its not a warning

--- Back-End/src/Routes/authController.js
+++ Back-End/src/Routes/authController.js
@@ -1,7 +1,17 @@
 const User = require("../models/User");
 const Role = require("../models/Role");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
+const { secret } = require("./config");
+
+const generateAccessToken = (id, roles) => {
+  const payload = {
+    id,
+    roles,
+  };
+  return jwt.sign(payload, secret, { expiresIn: "24h" });
+};
 
 class authController {
   async registration(req, res) {
@@ -28,15 +38,29 @@ class authController {
       });
       await user.save();
       return res.json({ message: "Пользователь успешно зарегистрирован" });
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({ token });
     } catch (e) {}
   }
   async login(req, res) {
     try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ message: `Пользователь ${username} не найден` });
+      }
+      const validPassword = bcrypt.compareSync(password, user.password);
+      if (!validPassword) {
+        return res.status(400).json({ message: `Введен неверный пароль` });
+      }
     } catch (e) {}
   }
   async getUser(req, res) {
     try {
-      res.json("server work!");
+      const users = await User.find();
+      res.json(users);
     } catch (e) {}
   }
 }
